45460753cfd233b9d96e93069853eba69213a944
jcarrharris <joncarrharris@gmail.com>
2013-09-12 10:45:46 -0400

    added tests

--- spec/controllers/article_controller_spec.rb
+++ spec/controllers/article_controller_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe ArticlesController do
 	it "should call all on Article" do
-		articles_controller use ArticleStub instead
+		get :index
+		
+		Article.should_receive(:all).and_return([])
 	end
 end
