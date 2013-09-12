1dac8fa24b275dc5382703e5c19dfb9a38d41e44
jcarrharris <joncarrharris@gmail.com>
2013-09-12 11:41:16 -0400

    added tests to cloned version

--- spec/controllers/article_controller_spec.rb
+++ spec/controllers/article_controller_spec.rb
@@ -2,6 +2,36 @@ require 'spec_helper'
 
 describe ArticlesController do
 	it "should call all on Article" do
-		articles_controller use ArticleStub instead
+		Article.should_receive(:all)
+		get :index
 	end
+
+	it "should assign articles to @articles" do
+		Article.should_receive(:all).and_return([])
+		get :index
+		expect(assign[:articles]).to eq([])
+	end
+
+	describe "#create" do
+		let(:article_params) {{"title" => "title", "body" -> "body"}}
+		let(:model) {FactoryGirl.build(:article)}
+
+		it "should call new on article" do
+
+			Article.should_receive(:new).and_return(model)
+
+			#Factory Girl way of doing it (good for numerous attributes)
+			#params = FactoryGirl.attributes_for(:article)			
+
+			post :create, "article" => article_params
+		end
+
+		it "should assign new article" do
+			Article.should_receive(:new).and_return(model)
+			post :create, "article" => article_params
+
+			expect(assigns[:article]).to eq(model)
+		end
+	end
+
 end
