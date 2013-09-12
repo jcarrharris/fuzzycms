bc1128e0e71aab0d17c5db0df9469736a92afc04
jcarrharris <joncarrharris@gmail.com>
2013-09-12 10:56:33 -0400

    added a few more tests

--- spec/controllers/article_controller_spec.rb
+++ spec/controllers/article_controller_spec.rb
@@ -2,8 +2,14 @@ require 'spec_helper'
 
 describe ArticlesController do
 	it "should call all on Article" do
+		Article.should_receive(:all)
 		get :index
-		
-		Article.should_receive(:all).and_return([])
 	end
+
+	it "should assign articles to @articles" do
+		Article.should_receive(:all).and_return([])
+		get :index
+		expect(assign[:articles]).to eq([])
+	end
+
 end
